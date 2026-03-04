7a8804c518532f17e01919167c3e4ecbfce9db1e
Artem <hrechuk.artem@gmail.com>
2026-03-04 15:24:09 +0100

    initial commit, created encrypt method

---
+++ encrypt.js
@@ -0,0 +1,76 @@
+import { randomBytes, scryptSync, createCipheriv } from "crypto";
+import fs from "fs";
+import { stdout, stdin } from "process";
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+const params = process.argv.slice(2);
+const command = params[0];
+const filePath = params[1];
+let algo = null;
+const algmMethodIndex = params.findIndex((p) => p === "--algo");
+if (algmMethodIndex !== -1) {
+  algo = params[algmMethodIndex + 1];
+}
+
+if (!command || !filePath) {
+  console.log(
+    "Usage: node encrypt.js <encrypt|decrypt> <file> [--algo aes-256]",
+  );
+
+  process.exit(1);
+}
+
+if (command !== "encrypt" && command !== "decrypt") {
+  console.log(`Unknown command: ${command}`);
+
+  process.exit(1);
+}
+
+function encrypt(filePath, algo) {
+  const salt = randomBytes(16);
+  const iv = randomBytes(16);
+
+  const fileContent = fs.readFileSync(filePath);
+
+  rl.question("Enter password: \n", (password) => {
+    const key = scryptSync(password, salt, 32);
+
+    const cipher = createCipheriv("aes-256-cbc", key, iv);
+
+    const encrypted = Buffer.concat([
+      cipher.update(fileContent),
+      cipher.final(),
+    ]);
+
+    const result = Buffer.concat([salt, iv, encrypted]);
+
+    const outPath = filePath + ".enc";
+
+    fs.writeFileSync(outPath, result);
+
+    console.log(`Created: ${outPath}`);
+
+    rl.close();
+  });
+
+  rl.stdoutMuted = true;
+  rl._writeToOutput = (char) => {
+    if (rl.stdoutMuted) process.stdout.write("*");
+    else process.stdout.write(char);
+  };
+}
+
+function decrypt(filePath) {
+  console.log(`Decrypting ${filePath}`);
+}
+
+if (command === "encrypt") {
+  encrypt(filePath, algo);
+} else {
+  decrypt(filePath);
+}
